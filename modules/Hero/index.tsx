3cd09c2eff16edf31a9591b84f7660002b26d268
jesky <jesky.yeremi@gmail.com>
2023-08-11 06:44:38 +0700

    feat(hero): added background music

--- modules/Hero/index.tsx
+++ modules/Hero/index.tsx
@@ -13,19 +13,45 @@ function Intro() {
   const [vec] = useState(() => new Vector3());
   return useFrame((state) => {
     state.camera.position.lerp(
-      vec.set(state.mouse.x * 5, 3 + state.mouse.y * 2, 14),
+      vec.set(state.mouse.x * 2, 3 + state.mouse.y * 2, 14),
       0.05
     );
     state.camera.lookAt(0, 0, 0);
   });
 }
 
-export default function Hero() {
+export default function HeroCanvas() {
   return (
     <Box sx={{ height: '100vh', width: '100%' }}>
+      <Box
+        sx={{
+          position: 'absolute' as 'absolute',
+          bottom: 0,
+          right: {
+            xs: '50%',
+            sm: 0,
+          },
+          transform: {
+            xs: 'translate(50%, -25%)',
+            sm: 'translate(-10%, -25%)',
+          },
+          zIndex: 1,
+          opacity: 0.25,
+          transition: 'all 0.3s ease',
+          '&:hover': {
+            opacity: 1,
+            cursor: 'pointer',
+          },
+        }}
+      >
+        <audio controls autoPlay loop>
+          <source src='/BGM.mp3' type='audio/mpeg' />
+        </audio>
+      </Box>
       <Canvas
+        dpr={[1.5, 2]}
         gl={{ alpha: false }}
-        camera={{ position: [0, 3, 100], fov: 15 }}
+        camera={{ position: [0, 100, 100], fov: 15 }}
         linear
       >
         <color attach='background' args={['black']} />
