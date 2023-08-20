c5f99204d1334e0b9aa62fb5981fa5a6a898a9bc
jesky <jesky.yeremi@gmail.com>
2023-08-20 22:13:38 +0700

    feat(responsive): optimized modal responsiveness

--- components/Layout/ProgressBar.tsx
+++ components/Layout/ProgressBar.tsx
@@ -19,7 +19,7 @@ export default function ProgressBar(props: ProgressBarProps) {
       <Grid
         component='div'
         sx={{
-          height: '1rem',
+          height: '0.5rem',
           width: '100%',
           display: 'flex',
           justifyContent: 'space-between',
@@ -27,18 +27,20 @@ export default function ProgressBar(props: ProgressBarProps) {
         }}
       >
         <Grid item>
-          {props.progress !== 0 && props.progress !== 100 && (
+          {props.progress !== 0 && (
             <Typography sx={{ fontSize: '0.75rem', color: '#ffffff' }}>
-              {loadingItem.length > 20
+              {loadingItem.length > 20 && props.progress < 100
                 ? `Loading: ${loadingItem.substring(0, 20)}...`
-                : loadingItem.length > 0
+                : loadingItem.length > 0 && props.progress < 100
                 ? `Loading: ${loadingItem}`
+                : props.progress >= 100
+                ? `Loading: completed`
                 : `Initializing Loader`}
             </Typography>
           )}
         </Grid>
         <Grid item>
-          {props.progress !== 0 && props.progress !== 100 && (
+          {props.progress !== 0 && (
             <Typography sx={{ fontSize: '0.75rem', color: '#ffffff' }}>
               {isNaN(props.progress) ? '0%' : `${Math.floor(props.progress)}%`}
             </Typography>
